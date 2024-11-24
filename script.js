bd94b730e1076f1f4b07bc183564d39cef9ac93a
gaurav-08-patel <gauravpatelrounak@gmail.com>
2024-11-24 22:37:55 +0545

    added qr generator

---
+++ script.js
@@ -0,0 +1,46 @@
+let input = document.querySelector(".header input");
+let sizes = document.querySelector("#sizes");
+let generateQr = document.querySelector(".generate-qr");
+let downloadQr = document.querySelector(".downlaod-qr");
+let qrContainer = document.querySelector(".qr-body");
+
+
+// let size = sizes.value;
+
+sizes.addEventListener("change",()=>{
+    textEmptyOrNot();
+})
+
+generateQr.addEventListener("click",(e)=>{
+    e.preventDefault();
+    textEmptyOrNot();
+})
+
+downloadQr.addEventListener("click",(e)=>{
+    let qrImage = document.querySelector(".qr-body img");
+
+    if(qrImage !== null){
+        let attribute = qrImage.getAttribute("src");
+        downloadQr.setAttribute("href",attribute);
+    }else{
+        alert("Generate the img first !")
+    }
+    
+})
+
+function textEmptyOrNot(){
+    input.value.length > 0 ? generateQrCode() : alert("Enter the text or URL in input area !"); ;
+
+}
+
+function generateQrCode(){
+    qrContainer.innerHTML="";
+    let size = sizes.value;
+    new QRCode(qrContainer,{
+        text:input.value,
+        height:size,
+        width:size,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+}
